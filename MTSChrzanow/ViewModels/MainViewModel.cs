32bb2369f7965c03532786ed37295d1274c69623
artysta <adrian.kurek93@gmail.com>
2020-07-28 19:04:03 +0200

    Add viewmodel for main page

---
+++ MTSChrzanow/ViewModels/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using MTSChrzanow.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MTSChrzanow.ViewModels
+{
+	class MainViewModel : BaseViewModel
+	{
+        ObservableCollection<MTSPost> _mtsPosts;
+        public ObservableCollection<MTSPost> MTSPosts
+        {
+            get { return _mtsPosts; }
+            set
+            {
+                _mtsPosts = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            MTSPosts = new ObservableCollection<MTSPost>();
+        }
+
+        public void GetPosts()
+		{
+
+		}
+    }
+}
