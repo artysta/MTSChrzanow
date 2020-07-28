f639e5973b8944ff922550b928f55a8fcd957f56
artysta <adrian.kurek93@gmail.com>
2020-07-28 19:03:26 +0200

    Add base viewmodel

---
+++ MTSChrzanow/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MTSChrzanow.ViewModels
+{
+	public abstract class BaseViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected BaseViewModel() {}
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this,
+			new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
