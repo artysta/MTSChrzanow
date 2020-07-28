5b6d47686b26ddb50a5f98708954ef9a7a167d8e
artysta <adrian.kurek93@gmail.com>
2020-07-28 19:02:14 +0200

    Add simple model for blog posts

---
+++ MTSChrzanow/Models/MTSPost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MTSChrzanow.Models
+{
+	public class MTSPost
+	{
+		public long Id { get; set; }
+		public DateTime Date { get; set; }
+		public string Link { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public string[] Tags { get; set; }
+		
+		public override string ToString() =>
+			$"Id: { Id }, { Date }" +
+			$" { Title }" +
+			$" { Content }";
+	}
+}
