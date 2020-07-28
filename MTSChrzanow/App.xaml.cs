af2c145c9fc1c158aee9cdac358b93406fdb5805
artysta <adrian.kurek93@gmail.com>
2020-07-28 18:53:19 +0200

    Add code that reads config file

--- MTSChrzanow/App.xaml.cs
+++ MTSChrzanow/App.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -6,11 +11,38 @@ namespace MTSChrzanow
 {
 	public partial class App : Application
 	{
+		public static string MTSChrzanowApiUrl { get; private set; }
+		public static string MTSChrzanowApiPostsUrl { get; private set; }
+
 		public App()
 		{
 			InitializeComponent();
+			InitializeApp();
+		}
 
+		private async Task InitializeApp()
+		{
+			await LoadConfig();
 			MainPage = new MainPage();
+		}
+
+		private static async Task LoadConfig()
+		{
+			var assembly = Assembly.GetAssembly(typeof(App));
+			var resourceNames = assembly.GetManifestResourceNames();
+			var configName = resourceNames.FirstOrDefault(s => s.Contains("config.json"));
+
+			using (var stream = assembly.GetManifestResourceStream(configName))
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					var json = await reader.ReadToEndAsync();
+					var dynamicJson = JObject.Parse(json);
+
+					MTSChrzanowApiUrl = dynamicJson["MTSChrzanowApiUrl"].Value<string>();
+					MTSChrzanowApiPostsUrl = dynamicJson["MTSChrzanowApiPostsUrl"].Value<string>();
+				}
+			}
 		}
 
 		protected override void OnStart()
