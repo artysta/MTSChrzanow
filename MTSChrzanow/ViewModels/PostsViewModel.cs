5c0e155de3c553eadcd9d64487fee079eb029f3f
artysta <adrian.kurek93@gmail.com>
2020-07-31 17:58:00 +0200

    Just rename

---
+++ MTSChrzanow/ViewModels/PostsViewModel.cs
@@ -0,0 +1,100 @@
+﻿using MTSChrzanow.Models;
+using System.Collections.ObjectModel;
+using System.Net;
+using Android.Util;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+using MTSChrzanow.Views;
+using Android.Content.Res;
+using System;
+
+namespace MTSChrzanow.ViewModels
+{
+	class PostsViewModel : BaseViewModel
+	{
+		private INavigation _navigation;
+		private ObservableCollection<MTSPost> _mtsPosts;
+		private ICommand _goToDetailsCommand;
+		private ICommand _pickerSelectionChanged;
+
+		public ICommand GoToDetailsCommand => _goToDetailsCommand ?? (_goToDetailsCommand = new Command<MTSPost>(OnGoToDetails));
+		public ICommand PickerSelectionChanged => _pickerSelectionChanged ?? (_pickerSelectionChanged = new Command<int>(OnSelectionChanged));
+
+		private async void OnGoToDetails(MTSPost item)
+		{
+			await _navigation.PushAsync(new DetailsPage(item));
+		}
+
+		private void OnSelectionChanged(int selectedPage)
+		{
+			GetPostsFromPage(selectedPage + 1);
+		}
+
+		public ObservableCollection<MTSPost> MTSPosts
+		{
+			get { return _mtsPosts; }
+			set
+			{
+				SetProperty(ref _mtsPosts, value);
+			}
+		}
+
+		public string[] PagePickerItems { set; get; }
+
+		public PostsViewModel() { }
+
+		public PostsViewModel(INavigation navigation)
+		{
+			_navigation = navigation;
+			InitializePages();
+		}
+
+		private void SetPosts(List<MTSPost> posts)
+		{
+			if (posts != null) MTSPosts = new ObservableCollection<MTSPost>(posts);
+		}
+
+		// Initialize pages when te app starts.
+		private void InitializePages()
+		{
+			WebClient client = new WebClient();
+			string query = GetQuery(App.MTSChrzanowApiUrl, App.MTSChrzanowApiPostsUrl);
+			string json = client.DownloadString(query);
+
+			// Deserialize blog posts.
+			var posts = JsonConvert.DeserializeObject<List<MTSPost>>(json);
+			SetPosts(posts);
+
+			// Get total amount of pages and initialize picker values.
+			WebHeaderCollection headers = client.ResponseHeaders;
+			int totalPages = int.Parse(headers["X-WP-TotalPages"]);
+			SetPickerItemsValues(totalPages);
+		}
+
+		private async void GetPostsFromPage(int pageNumber)
+		{
+			string query = GetQuery(App.MTSChrzanowApiUrl, App.MTSChrzanowApiPostsFromPageUrl, pageNumber.ToString());
+			string json = await new WebClient().DownloadStringTaskAsync(query);
+
+			// Deserialize blog posts.
+			var posts = JsonConvert.DeserializeObject<List<MTSPost>>(json);
+			SetPosts(posts);
+		}
+
+		private void SetPickerItemsValues(int k)
+		{
+			PagePickerItems = new string[k];
+			for (int i = 0; i < k; i++) PagePickerItems[i] = "Page " + (i + 1);
+		}
+
+		private string GetQuery(params string[] list)
+		{
+			StringBuilder sb = new StringBuilder();
+			foreach (string s in list) sb.Append(s);
+			return sb.ToString();
+		}
+	}
+}
---
+++ MTSChrzanow/Views/PostsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using MTSChrzanow.Models;
+using MTSChrzanow.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MTSChrzanow.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class PostsPage : ContentPage
+	{
+		PostsViewModel viewModel;
+		public PostsPage()
+		{
+			InitializeComponent();
+			viewModel = new PostsViewModel(Navigation);
+			BindingContext = viewModel;
+		}
+
+		void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			viewModel.GoToDetailsCommand.Execute(e.Item as MTSPost);
+		}
+
+		void Picker_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			Picker p = (Picker)sender;
+			viewModel.PickerSelectionChanged.Execute(p.SelectedIndex);
+		}
+	}
+}
