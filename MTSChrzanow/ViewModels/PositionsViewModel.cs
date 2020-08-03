8db65289a2657a9d6a32b988f700e86103539ce4
artysta <adrian.kurek93@gmail.com>
2020-08-03 18:09:40 +0200

    Add files for positions page

---
+++ MTSChrzanow/ViewModels/PositionsViewModel.cs
@@ -0,0 +1,59 @@
+﻿using Android.Content.Res;
+using MTSChrzanow.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using MTSChrzanow.Models;
+
+namespace MTSChrzanow.ViewModels
+{
+	public class PositionsViewModel : BaseViewModel
+	{
+		private ObservableCollection<PositionsMenuItem> _menuItems;
+		private INavigation _navigation;
+		private ICommand _goToPositionCommand;
+		
+		public ICommand GoToSelectedPage => _goToPositionCommand ?? (_goToPositionCommand = new Command<PositionsMenuItem>(OnPositionItemClicked));
+
+		public PositionsViewModel() : this(null)	{ }
+
+		public PositionsViewModel(INavigation navigation)
+		{
+			_navigation = navigation;
+			InitializeMenu();
+		}
+
+		public void InitializeMenu()
+		{
+			List<PositionsMenuItem> items = new List<PositionsMenuItem>
+			{
+				new PositionsMenuItem { Type = PositionsMenuItem.MenuItemType.Goalkeepers, Title = "Bramkarze" },
+				new PositionsMenuItem { Type = PositionsMenuItem.MenuItemType.Fullbacks, Title = "Rozgrywający" },
+				new PositionsMenuItem { Type = PositionsMenuItem.MenuItemType.CircleRunners, Title = "Obrotowi" },
+				new PositionsMenuItem { Type = PositionsMenuItem.MenuItemType.Wingers, Title = "Skrzydłowi" },
+			};
+
+			MenuItems = new ObservableCollection<PositionsMenuItem>(items);
+		}
+
+		public ObservableCollection<PositionsMenuItem> MenuItems
+		{
+			get { return _menuItems; }
+			set
+			{
+				SetProperty(ref _menuItems, value);
+			}
+		}
+		
+		private async void OnPositionItemClicked(PositionsMenuItem item)
+		{
+			if (item == null)
+				return;
+
+			await _navigation.PushAsync(new PlayersPage(item.Type));
+		}
+	}
+}
---
+++ MTSChrzanow/Views/PositionsPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Android.Webkit;
+using MTSChrzanow.Models;
+using MTSChrzanow.ViewModels;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MTSChrzanow.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class PositionsPage : ContentPage
+	{
+		private PositionsViewModel viewModel;
+
+		public PositionsPage()
+		{
+			InitializeComponent();
+			viewModel = new PositionsViewModel(Navigation);
+			BindingContext = viewModel;
+		}
+
+		void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			viewModel.GoToSelectedPage.Execute(e.Item as PositionsMenuItem);
+		}
+	}
+}
