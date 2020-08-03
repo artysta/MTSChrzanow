8f0be6fbd7a3b012befc57f156c9e5f6dfd6696d
artysta <adrian.kurek93@gmail.com>
2020-08-03 18:07:36 +0200

    Add positions menu item model

---
+++ MTSChrzanow/Models/PositionsMenuItem.cs
@@ -0,0 +1,18 @@
+﻿namespace MTSChrzanow.Models
+{
+	public class PositionsMenuItem
+	{
+		public enum MenuItemType
+		{
+			Goalkeepers,
+			Fullbacks,
+			CircleRunners,
+			Wingers
+		}
+
+		public MenuItemType Type { get; set; }
+		public string Title { get; set; }
+
+		public override string ToString() => Title;
+	}
+}
