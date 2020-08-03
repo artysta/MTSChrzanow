bc1691fef743a097993403861be22cff04c3fade
artysta <adrian.kurek93@gmail.com>
2020-08-03 18:11:50 +0200

    Add files for player details page

---
+++ MTSChrzanow/ViewModels/PlayerDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MTSChrzanow.Models;
+
+namespace MTSChrzanow.ViewModels
+{
+	public class PlayerDetailsViewModel : BaseViewModel
+	{
+		private MTSPlayer _mtsPlayer;
+		public MTSPlayer MTSPlayer
+		{
+			get => _mtsPlayer;
+			set
+			{
+				SetProperty(ref _mtsPlayer, value);
+			}
+		}
+
+		public PlayerDetailsViewModel() { }
+
+		public PlayerDetailsViewModel(MTSPlayer player)
+		{
+			MTSPlayer = player;
+		}
+	}
+}
---
+++ MTSChrzanow/Views/PlayerDetailsPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MTSChrzanow.Models;
+using MTSChrzanow.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MTSChrzanow.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class PlayerDetailsPage : ContentPage
+	{
+		public PlayerDetailsPage(MTSPlayer player)
+		{
+			InitializeComponent();
+			BindingContext = new PlayerDetailsViewModel(player);
+		}
+	}
+}
