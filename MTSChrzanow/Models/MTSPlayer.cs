f6d7452cb5e39da4757bd5ee1c4894f688916030
artysta <adrian.kurek93@gmail.com>
2020-08-03 18:02:03 +0200

    Add player model

---
+++ MTSChrzanow/Models/MTSPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTSChrzanow.Models
+{
+    public class MTSPlayer
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string Position { get; set; }
+        public string Nationality { get; set; }
+        public string BirthDate { get; set; }
+        public double Height { get; set; }
+        public int Weight { get; set; }
+        public override string ToString() => $"{ Name }, { Number }, { Position }, { Nationality }, { BirthDate }, { Height } cm, { Weight } kg.";
+    }
+}
