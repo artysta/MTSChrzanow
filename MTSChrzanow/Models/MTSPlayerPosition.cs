fa491b88f1ddb5a5c5c8d7a9ca9e37adb2aa803a
artysta <adrian.kurek93@gmail.com>
2020-08-03 18:02:40 +0200

    Add player position class

---
+++ MTSChrzanow/Models/MTSPlayerPosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTSChrzanow.Models
+{
+    public class MTSPlayerPosition
+    {
+        public static string Goalkeeper => "Bramkarz";
+        public static string Fullback => "Rozgrywający";
+        public static string CircleRunner => "Obrotowy";
+        public static string Winger => "Skrzydłowy";
+    }
+}
