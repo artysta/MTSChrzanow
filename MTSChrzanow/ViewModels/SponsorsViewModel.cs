2e92e77b99cb749a852be3058525ec0b960ec668
artysta <adrian.kurek93@gmail.com>
2020-08-04 17:46:37 +0200

    Add files for sponsors list page

---
+++ MTSChrzanow/ViewModels/SponsorsViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Android.Content.Res;
+using MTSChrzanow.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using MTSChrzanow.Models;
+
+namespace MTSChrzanow.ViewModels
+{
+	public class SponsorsViewModel : BaseViewModel
+	{
+		private ObservableCollection<MTSSponsor> _mtsSponsors;
+		private INavigation _navigation;
+		private ICommand _goToSponsorCommand;
+		
+		public ICommand GoToSelectedSponsor => _goToSponsorCommand ?? (_goToSponsorCommand = new Command<MTSSponsor>(OnSponsorItemClicked));
+
+		public ObservableCollection<MTSSponsor> MTSSponsors
+		{
+			get { return _mtsSponsors; }
+			set
+			{
+				SetProperty(ref _mtsSponsors, value);
+			}
+		}
+
+		public SponsorsViewModel() : this(null)	{ }
+
+		public SponsorsViewModel(INavigation navigation)
+		{
+			_navigation = navigation;
+			InitializeSponsors();
+		}
+
+		public void InitializeSponsors()
+		{
+			List<MTSSponsor> items = MTSSponsorsData.GetMTSSponsors();
+
+			MTSSponsors = new ObservableCollection<MTSSponsor>(items);
+		}
+		
+		private async void OnSponsorItemClicked(MTSSponsor sponsor)
+		{
+			if (sponsor == null)
+				return;
+
+			await _navigation.PushAsync(new SponsorDetailsPage(sponsor));
+		}
+	}
+}
---
+++ MTSChrzanow/Views/SponsorsPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MTSChrzanow.Models;
+using MTSChrzanow.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MTSChrzanow.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class SponsorsPage : ContentPage
+	{
+		private SponsorsViewModel viewModel;
+
+		public SponsorsPage()
+		{
+			InitializeComponent();
+			viewModel = new SponsorsViewModel(Navigation);
+			BindingContext = viewModel;
+		}
+
+		void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			viewModel.GoToSelectedSponsor.Execute(e.Item as MTSSponsor);
+		}
+	}
+}
