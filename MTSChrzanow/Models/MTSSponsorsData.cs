a81ff1eea0b3a4096a62118252664a744279bf69
artysta <adrian.kurek93@gmail.com>
2020-08-04 17:44:56 +0200

    Add class that provides sponsors data

---
+++ MTSChrzanow/Models/MTSSponsorsData.cs
@@ -0,0 +1,328 @@
+﻿using System.Collections.Generic;
+
+namespace MTSChrzanow.Models
+{
+	public class MTSSponsorsData
+	{
+		public static List<MTSSponsor> GetMTSSponsors()
+		{
+			List<MTSSponsor> list = new List<MTSSponsor>();
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Gmina Chrzanów",
+					Description = "Gmina Chrzanów zajmuje powierzchnię 79,44 km2. Tworzy ją miasto Chrzanów (38,32 km2) oraz sołectwa (41,12 km2) Balin, Luszowice, Pogorzyce, Płaza, Źrebce i Okradziejówka. Chrzanów graniczy z gminami: Jaworznem, Trzebinią, Libiążem, Babicami i Alwernią.Pod względem położenia geograficznego gmina zlokalizowana jest w południowej części Polski, w województwie małopolskim, na pograniczu Wyżyny Śląskiej i Wyżyny Krakowsko-Częstochowskiej, na małym grzbiecie jurajskim u wylotu Rowu Krzeszowickiego, nad lewym dopływem Wisły – rzeką Chechło.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Aleja Henryka 20",
+					PhoneNumber = "+48 (32) 75 85 152",
+					Mail = "sekretariat@chrzanow.pl",
+					WebSite = "https://www.chrzanow.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Unimetal Recycling Sp. z o.o.",
+					Description = "Unimetal Recycling Sp. z o.o. to firma, która od 20 lat z powodzeniem zajmuje się skupem, a przede wszystkim recyklingiem zużytych katalizatorów pochodzących z samochodów osobowych, ciężarówek, autobusów, maszyn rolniczych, górniczych oraz przemysłowych.Wszystko po to, by odzyskać zawarte w nich metale szlachetne – platynę, pallad oraz rod, które trafią do ponownego wykorzystania nie tylko w motoryzacji. Dzięki temu zyskuje zarówno przemysł jak i środowisko.Recykling odbywa się na najwyższym poziomie, a to co decyduje o naszym sukcesie, to doświadczona i wykwalifikowana kadra oraz bogate zaplecze techniczne. Posiadamy specjalistyczne laboratorium ze spektrometrami najnowszej generacji – Olympus oraz ICP MS, które precyzyjnie określają zawartość pierwiastków szlachetnych w dostarczonej partii.Współpracujemy z największymi skupami katalizatorów i stacjami demontażu pojazdów w Polsce i Europie. W 2018 roku do życia została powołana Fundacja Unimetal Recykling, której zadaniem jest realizacja planów wynikających z polityki CSR firmy.",
+					City = "Trzebinia",
+					PostalCode = "32-540",
+					Street = "Tuwima 21",
+					PhoneNumber = "+48 730 016 017",
+					Mail = "info@unimetalrecycling.pl",
+					WebSite = "https://unimetalrecycling.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "MANT",
+					Description = "Świadczymy usługi szeroko pojętego marketingu internetowego. Na rynku usług internetowych istniejemy od 1 czerwca 2008 roku. Od samego początku dostrzegamy potencjał drzemiący w tej formie promocji, ze szczególnym uwzględnieniem pozycjonowania stron internetowych w najpopularniejszej wyszukiwarce – Google oraz social media.",
+					City = "Trzebinia",
+					PostalCode = "32-540",
+					Street = "Sikorskiego 71",
+					PhoneNumber = "(32) 442 00 07",
+					Mail = "biuro@mant.pl",
+					WebSite = "https://mant.pl"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Chrzanowska Izba Gospodarcza",
+					Description = "Chrzanowska Izba Gospodarcza powstała w roku 2001 jako Stowarzyszenie Przedsiębiorców Prywatnych w Trzebini, zrzeszające podmioty gospodarcze z regionu Powiatu Chrzanowskiego.Od początku swojego istnienia podejmuje działania zmierzające do rozwoju przedsiębiorczości w różnych formach organizacyjnych, gromadzi i upowszechnia informacje dotyczące nawiązywania kontaktów gospodarczych, udziela podmiotom gospodarczym pomocy w rozwiązywaniu problemów. Wszelkie działania podejmowane przez Chrzanowską Izbę Gospodarczą ukierunkowane są na wspieranie i promocję firm członkowskich ale nie tylko – w wielu organizowanych przez nas projektach uczestniczą także firmy niezrzeszone, instytucje i różne organizacje.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Rynek 16",
+					PhoneNumber = "+48 (32) 754 15 65",
+					Mail = "biuro@chrzanowskaizba.pl",
+					WebSite = "http://www.chrzanowskaizba.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "DS Auto Sp. z o.o.",
+					Description = "DS AUTO jest od 20 lat największym w Polsce skupem katalizatorów. Posiadamy ponad 70 punktów partnerskich w całym kraju oraz 17 w pozostałych krajach europejskich. Miesięcznie skupujemy ponad 32 tysiące zużytych katalizatorów, które zostają poddane recyklingowi.Na naszej stronie internetowej znajduje się cennik zawierający ponad 7 500 modeli katalizatorów z aktualnymi cenami. Oferujemy również możliwość wyceny na facebooku poprzez Messenger. W 2018 roku założyliśmy skup wysyłkowy – detaliczny oraz hurtowy, czat na stronie czy infolinię.W związku z wprowadzonym programem Zdrowe Relacje Biznesowe, wspieramy nieustannie skupy, które chcą rozwijać swój biznes. Pomagamy w doborze odpowiednich narzędzi do pracy, oferujemy również nasze własne – tablet z autorską aplikacją CatCatalog do profesjonalnej wyceny. Wspieramy marketingowo oraz edukacyjnie.Zapraszamy do współpracy wszystkie skupy katalizatorów oraz stacje demontażu pojazdów, które chcą współpracować z najlepszymi odbiorcami na rynku. Z kolei wszystkich, którzy mają do sprzedania katalizator, zapraszamy serdecznie na naszą stronę, gdzie znajdują się dane kontaktowe wszystkich skupów partnerskich w Polsce.",
+					City = "Trzebinia",
+					PostalCode = "32-540",
+					Street = "Tuwima 21",
+					PhoneNumber = "+48 604 611 917",
+					Mail = "dsautospzoo@wp.pl",
+					WebSite = "https://katalizatorychrzanow.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Duhabex Sp. z o.o.",
+					Description = "Duhabex Sp. z o.o. to firma świadcząca usługi transportowe krajowe i międzynarodowe.",
+					City = "Trzebinia",
+					PostalCode = "32-540",
+					Street = "Harcerska 10",
+					PhoneNumber = "+48 (32) 624 0 624",
+					Mail = "kontakt@duhabex.pl",
+					WebSite = "https://duhabex.pl"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "PRBM CEM-BUD Sp. z o.o.",
+					Description = "Przedsiębiorstwo Remontowo – Budowlano – Montażowe „CEM-BUD” Sp. z o.o. zajmuje się usługami remontowo – budowlano – montażowymi na rzecz przemysłu ze szczególnym uwzględnieniem przemysłu cementowo – wapienniczego oraz osób fizycznych.Firma posiada kadrę pracowników o najwyższych kwalifikacjach i wieloletnim stażu mająca doświadczenie w pracy z wieloma firmami na terenie Unii Europejskiej w dziedzinie budownictwa i transportu.PRBM Cem-Bud należy do największych dystrybutorów wszystkich rodzajów cementów, za co firma została wielokrotnie wyróżniona między innymi Nagrodą Kazimierza Wielkiego ‘2005 oraz tytułem “Efektywna Firma” w rankingu sporządzanym przez Instytut Nowoczesnego Biznesu oraz nagrodą “Gepard Biznesu”.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Śląska 42",
+					PhoneNumber = "+48 (32) 623 34 45",
+					Mail = "biuro@prbmcembud.pl",
+					WebSite = "http://prbmcembud.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Centrum Medyczne IGAMED",
+					Description = "Centrum Medyczne IGAMED to nowo powstała, prywatna placówka o szerokim zakresie usług medycznych.",
+					City = "Chranów",
+					PostalCode = "32-500",
+					Street = "Oświęcimska 3",
+					PhoneNumber = "+48 (32) 728 06 06",
+					Mail = "rejestracja@igamed.pl",
+					WebSite = "https://www.igamed.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "PSB Mrówka - Chrzanów",
+					Description = "Market Dekoracyjno-Budowlany Mrówka Chrzanów powstał w Chrzanowie w 2012 r. Jest to sklep samoobsługowy w całości składający się z polskiego kapitału. Należymy do Grupy Polskich Składów Budowlanych S.A. działających na rynku od 20 lat. Grupa PSB jest największą i najszybciej rozwijającą się siecią hurtowni materiałów budowlanych oraz marketów „dom i ogród” w Polsce. Sklep posiada w ofercie kilkanaście tysięcy pozycji asortymentowych, które podzielone są tematycznie na 12 działów:Dział BudowlanyDział Chemii GospodarczejDział DrzwiDział DekoracyjnyDział ElektronarzędziDział FarbDział KuchennyDział OgródDział OświetlenieDział SamochodowyDział SanitarnyDział AGDZdobywane przez lata doświadczenie pozwala nam dziś zaoferować nie tylko dobre produkty w dobrych cenach, ale także służyć pomocą w wyborze odpowiedniego towaru oraz rzetelną odpowiedzią na zadawane pytania. W sklepie Mrówka raz w miesiącu organizowane są promocje, o których klienci informowani są za pomocą gazetek promocyjnych. Oferta promocyjna jest dobrana tak, aby odpowiadać na bieżące potrzeby klient. Staramy się, aby atmosfera naszego sklepu była kameralna, by sprzedawca był blisko Państwa i mógł dopomóc w wyborze produktów fachową radą.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Kroczymiech 24",
+					PhoneNumber = "+48 (32) 623 59 61",
+					Mail = "chrzanow@psbmrowka.com.pl",
+					WebSite = "https://www.mrowkachrzanow.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Restauracja McDonald's - Chrzanów",
+					Description = "N/A",
+					City = "Chrzanów",
+					PostalCode = " 32-500",
+					Street = "Trzebińska 71",
+					PhoneNumber = "+48 664 718 101",
+					Mail = "N/A",
+					WebSite = "N/A"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "ESSA",
+					Description = "Frima ESSA skupia się i specjalizuje w wynajmie i sprzedaży własnych nieruchomości pod dedykowaną marką ESSA.",
+					City = "Trzebinia",
+					PostalCode = "32-540",
+					Street = "Kopalniana 2a",
+					PhoneNumber = "+48 530 200 233",
+					Mail = "biuro@domosnieruchomosci.pl",
+					WebSite = "http://essanieruchomosci.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Artyzan Marcin Skwarek - KreatywneMaluchy.pl",
+					Description = "Firma KreatywneMaluchy.pl zajmuje się sprzedażą artykułów papierniczych i zabawek dla dzieci.",
+					City = "Trzebinia",
+					PostalCode = "32-540",
+					Street = "Kopalniana 2a",
+					PhoneNumber = "+48 (32) 720 69 14",
+					Mail = "sklep@kreatywnemaluchy.pl",
+					WebSite = "https://kreatywnemaluchy.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Belfer-Sport. s.c. FUH",
+					Description = "Belfer-Sport. s.c. FUH jest firmą zajmującą się sprzedażą artykułów sportowych.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Kusocińskiego 4",
+					PhoneNumber = "+48 (32) 623 05 07",
+					Mail = "N/A",
+					WebSite = "N/A"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Kamiński Ryszard. Zakład elektroinstalacyjny.",
+					Description = "Zakład elektroinstalacyjny zajmuje się montażem i obsługą instalacji elektrycznych.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Stara Huta 33",
+					PhoneNumber = "+48 (32) 623 48 33",
+					Mail = "N/A",
+					WebSite = "N/A"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Ośrodek Szkolenia Kierowców L4you",
+					Description = "Ośrodek Szkolenia Kierowców L4you świadczy usługi w zakresie szkolenia kierowców.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Matejki 2/5",
+					PhoneNumber = "+48 600 181 065",
+					Mail = "osk@l4you.pl",
+					WebSite = "http://www.l4you.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Powiat Chrzanowski",
+					Description = "Powiat chrzanowski zlokalizowany jest w północno-zachodniej części województwa małopolskiego na pograniczu Wyżyny Krakowsko-Częstochowskiej oraz Śląskiej. Posiada bardzo dogodne położenie geograficzne. Leży pomiędzy dwoma miastami wojewódzkimi, Krakowem i Katowicami. Stolicą powiatu jest miasto Chrzanów, umiejscowione w odległości około 50 km na północny zachód od Krakowa, oraz około 30 km na wschód od aglomeracji górnośląskiej. Dobrą komunikację zapewnia rozbudowany układ dróg, w tym przebiegająca przez powiat autostrada A4 oraz równoległa do niej linia kolejowa. Niedaleko od powiatu znajduj ą się dwa lotniska: jedno ponad 30 km od Chrzanowa, w podkrakowskich Balicach, a drugie w odległości ok. 65km- w Katowicach-Pyrzowicach.W skład powiatu wchodzą cztery gminy miejsko-wiejskie: Alwernia, Chrzanów, Libiąż, Trzebinia i jedna gmina wiejska Babice. Powiat zajmuje powierzchnię 371,5 km2. Zamieszkuje go ok. 129,1 tys. osób.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Partyzantów 2",
+					PhoneNumber = "+48 32 625-79-95",
+					Mail = "sekretariat@powiat-chrzanowski.pl",
+					WebSite = "http://www.powiat-chrzanowski.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Centrum Handlowe MAX",
+					Description = "Centrum Handlowe MAX udostępnia lokale dla firm usługowych.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Trzebińska 40",
+					PhoneNumber = "+48 (32) 623 96 48",
+					Mail = "kontakt@chmax.pl",
+					WebSite = "https://chmax.pl"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Debal s.c. Leszek Smoła i spółka",
+					Description = "Debal s.c. specjalizuje się w budowie domów szkieletowych zaczynając od stanu surowego aż do prac wykończeniowych.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Akacjowa 2",
+					PhoneNumber = "+48 501 619 980",
+					Mail = "leszek@debal.pl",
+					WebSite = "http://debal.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Kancelaria Radcy Prawnego Mateusz Mosor",
+					Description = "Kancelaria świadczy usługi prawne dla osób fizycznych oraz firm. Specjalnością kancelarii jest obsługa firm w wielu rodzajach działalności gospodarczej w zakresie prawnym.Kancelaria świadczy również profesjonalne usługi prawne dla osób fizycznych zarówno podczas składania pozwu jak i podczas obrony. Ponadto kancelaria świadczy szeroki zakres porad prawnych w różnych dziedzinach.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Słowackiego 4/4",
+					PhoneNumber = "+48 509 969 369",
+					Mail = "kancelariamosor@gmail.com",
+					WebSite = "http://kancelariamosor.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Drukarnia OPTIMA",
+					Description = "Drukarnia OPTIMA jest dostawcą usług expresowego druku cyfrowego, wielkoformatowego i kompleksowej reprografii dokumentów.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Jaworznicka 37a",
+					PhoneNumber = "+48 693 988 666",
+					Mail = "biuro@drukarniaoptima.pl",
+					WebSite = "https://drukarniaoptima.pl/"
+				}
+			);
+
+			list.Add
+			(
+				new MTSSponsor
+				{
+					Name = "Bodylove",
+					Description = "Firma Bodylove jest studiem treningu personalnego, oferujący treningi personalne różnego typu, między innymi treningi odchudzające, kształtujące sylwetkę i wiele innych.",
+					City = "Chrzanów",
+					PostalCode = "32-500",
+					Street = "Kardynała Stefana Wyszyńskiego 17A",
+					PhoneNumber = "+48 784 572 702",
+					Mail = "kontakt@joannatambor.pl",
+					WebSite = "https://joannatambor.pl/"
+				}
+			);
+			return list;
+		}
+	}
+}
