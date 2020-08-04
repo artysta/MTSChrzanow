b7bfed8bf1f425284f97df875a1a48eb8b196a0e
artysta <adrian.kurek93@gmail.com>
2020-08-04 17:44:17 +0200

    Add sponsor model

---
+++ MTSChrzanow/Models/MTSSponsor.cs
@@ -0,0 +1,15 @@
+﻿namespace MTSChrzanow.Models
+{
+	public class MTSSponsor
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string City { get; set; }
+		public string PostalCode { get; set; }
+		public string Street { get; set; }
+		public string PhoneNumber { get; set; }
+		public string Mail { get; set; }
+		public string WebSite { get; set; }
+		public override string ToString() => $"{ Name }, { Description }, { City }, { PostalCode }, { Street }, { PhoneNumber }, { Mail }, { WebSite }.";
+	}
+}
