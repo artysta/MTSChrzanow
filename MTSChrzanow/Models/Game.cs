9e195c489864811e322595544111ddec7aedf848
artysta <adrian.kurek93@gmail.com>
2020-08-05 19:02:19 +0200

    Add game (match) model

---
+++ MTSChrzanow/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MTSChrzanow.Models
+{
+	public class Game
+	{
+		public string Home { get; set; }
+		public string HomeGoal { get; set; }
+		public string Away { get; set; }
+		public string AwayGoal { get; set; }
+		public DateTime DateOfGame { get; set; }
+		public override string ToString() => $"Home { Home }, Away { Away }, Date of match { DateOfGame }";
+	}
+}
