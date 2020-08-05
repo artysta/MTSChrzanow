e2c0be538b69910d02b78627a03070eb37fb6080
artysta <adrian.kurek93@gmail.com>
2020-08-05 19:08:07 +0200

    Add files for game details page

---
+++ MTSChrzanow/ViewModels/GameDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MTSChrzanow.Models;
+
+namespace MTSChrzanow.ViewModels
+{
+	public class GameDetailsViewModel : BaseViewModel
+	{
+		private Game _game;
+		public Game Game
+		{
+			get => _game;
+			set
+			{
+				SetProperty(ref _game, value);
+			}
+		}
+
+		public GameDetailsViewModel() { }
+
+		public GameDetailsViewModel(Game game)
+		{
+			Game = game;
+		}
+	}
+}
