22408c56a74bfce882dda10384fb080bceac1596
artysta <adrian.kurek93@gmail.com>
2020-08-05 19:04:08 +0200

    Add class that provides team names

---
+++ MTSChrzanow/Models/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTSChrzanow.Models
+{
+	public class Team
+	{
+		public static string MTSChrzanow => "MTS Chrzanów";
+		public static string MKSOlimpiaMEDEX => "MKS Olimpia MEDEX Piekary Śląskie";
+		public static string SMSZPRPKielce => "SMS ZPRP Kielce";
+		public static string ORLENUpstreamCzuwaj => "ORLEN Upstream Czuwaj Przemyśl";
+		public static string MUKSZaglebie => "MUKS Zagłębie ZSO 14 Sosnowiec";
+		public static string KSSPR => "KSSPR Końskie";
+		public static string KSViret => "KS Viret CMC Zawiercie";
+		public static string MKSPadwa => "MKS Padwa Zamość";
+		public static string SPRWisla => "SPR Wisła Sandomierz";
+		public static string KSZOOstrowiec => "KSZO Ostrowiec Świętokrzyski";
+		public static string AZSUJK => "AZS UJK Kielce";
+	}
+}
