75cbfef8c78aa0ff41f07d56fa9a279fdc08a83b
artysta <adrian.kurek93@gmail.com>
2020-08-05 19:06:52 +0200

    Add files for games list page

---
+++ MTSChrzanow/ViewModels/GamesViewModel.cs
@@ -0,0 +1,52 @@
+﻿using MTSChrzanow.Models;
+using System.Collections.ObjectModel;
+using System.Net;
+using Android.Util;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Windows.Input;
+using Xamarin.Forms;
+using MTSChrzanow.Views;
+using Android.Content.Res;
+using System;
+using System.Threading.Tasks;
+
+namespace MTSChrzanow.ViewModels
+{
+	class GamesViewModel : BaseViewModel
+	{
+		private ObservableCollection<Game> _games;
+		private INavigation _navigation;
+		private ICommand _goToGameCommand;
+
+		public ICommand GoToGameDetailsCommand => _goToGameCommand ?? (_goToGameCommand = new Command<Game>(OnGameItemClicked));
+
+		public ObservableCollection<Game> Games
+		{
+			get { return _games; }
+			set
+			{
+				SetProperty(ref _games, value);
+			}
+		}
+
+		public GamesViewModel() : this(null) { }
+
+		public GamesViewModel(INavigation navigation)
+		{
+			_navigation = navigation;
+			InitializeGamesList();
+		}
+		public void InitializeGamesList()
+		{
+			List<Game> items = GamesData.GetGames();
+			Games = new ObservableCollection<Game>(items);
+		}
+
+		private async void OnGameItemClicked(Game game)
+		{
+			await _navigation.PushAsync(new GameDetailsPage(game));
+		}
+	}
+}
---
+++ MTSChrzanow/Views/GamesPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MTSChrzanow.Models;
+using MTSChrzanow.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MTSChrzanow.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class GamesPage : ContentPage
+	{
+		private GamesViewModel viewModel;
+
+		public GamesPage()
+		{
+			InitializeComponent();
+			viewModel = new GamesViewModel(Navigation);
+			BindingContext = viewModel;
+		}
+
+		void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			viewModel.GoToGameDetailsCommand.Execute(e.Item as Game);
+		}
+	}
+}
