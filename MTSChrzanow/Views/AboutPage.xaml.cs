0fd65fa43af3548719f3a6020610f201588878fc
artysta <adrian.kurek93@gmail.com>
2020-08-07 15:30:47 +0200

    Add "about appliaction" page

---
+++ MTSChrzanow/Views/AboutPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MTSChrzanow.Models;
+using MTSChrzanow.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MTSChrzanow.Views
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class AboutPage : ContentPage
+	{
+		public AboutPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
