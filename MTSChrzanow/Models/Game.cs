7b07047c8354cf4526466ff34a6cd9b9e7cf9331
artysta <adrian.kurek93@gmail.com>
2020-08-20 20:26:59 +0200

    Update: modify and add new properties, add json attributes

--- MTSChrzanow/Models/Game.cs
+++ MTSChrzanow/Models/Game.cs
@@ -1,14 +1,50 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
 
 namespace MTSChrzanow.Models
 {
 	public class Game
 	{
+		private string _homeGoal;
+		private string _awayGoal;
+		private string _dateOfGameString;
+		[JsonProperty("home")]
 		public string Home { get; set; }
-		public string HomeGoal { get; set; }
+		[JsonProperty("home-goal")]
+		public string HomeGoal
+		{
+			get => _homeGoal;
+			set
+			{
+				_homeGoal = value.Trim().Equals("") ? "-" : value;
+			}
+		}
+		[JsonProperty("away")]
 		public string Away { get; set; }
-		public string AwayGoal { get; set; }
+		[JsonProperty("away-goal")]
+		public string AwayGoal
+		{
+			get => _awayGoal;
+			set
+			{
+				_awayGoal = value.Trim().Equals("") ? "-" : value;
+			}
+		}
+		[JsonProperty("date-of-game")]
+		public string DateOfGameString
+		{
+			get => _dateOfGameString;
+			set
+			{
+				_dateOfGameString = value;
+				DateOfGame = DateTime.Parse(value, new CultureInfo("pl-PL"), DateTimeStyles.NoCurrentDateDefault);
+			}
+		}
 		public DateTime DateOfGame { get; set; }
-		public override string ToString() => $"Home { Home }, Away { Away }, Date of match { DateOfGame }";
+
+		[JsonProperty("is-finished")]
+		public bool IsFinished { get; set; }
+		public override string ToString() => $"Home { Home },Away { Away }, Date of match { DateOfGameString }, Is finished { IsFinished }";
 	}
 }
