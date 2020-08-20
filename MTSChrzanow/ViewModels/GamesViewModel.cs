85464366f91f70c2c4adf6695f3fc15dcda7e568
artysta <adrian.kurek93@gmail.com>
2020-08-20 20:30:03 +0200

    Add code that reads games data from db

--- MTSChrzanow/ViewModels/GamesViewModel.cs
+++ MTSChrzanow/ViewModels/GamesViewModel.cs
@@ -8,14 +8,13 @@ using System.Collections.Generic;
 using System.Windows.Input;
 using Xamarin.Forms;
 using MTSChrzanow.Views;
-using Android.Content.Res;
-using System;
 using System.Threading.Tasks;
 
 namespace MTSChrzanow.ViewModels
 {
 	class GamesViewModel : BaseViewModel
 	{
+		private bool _isBusy;
 		private ObservableCollection<Game> _games;
 		private INavigation _navigation;
 		private ICommand _goToGameCommand;
@@ -31,6 +30,15 @@ namespace MTSChrzanow.ViewModels
 			}
 		}
 
+		public bool IsBusy
+		{
+			get { return _isBusy; }
+			set
+			{
+				SetProperty(ref _isBusy, value);
+			}
+		}
+
 		public GamesViewModel() : this(null) { }
 
 		public GamesViewModel(INavigation navigation)
@@ -38,12 +46,21 @@ namespace MTSChrzanow.ViewModels
 			_navigation = navigation;
 			InitializeGamesList();
 		}
-		public void InitializeGamesList()
+		public async void InitializeGamesList()
 		{
-			List<Game> items = GamesData.GetGames();
-			Games = new ObservableCollection<Game>(items);
+			IsBusy = true;
+			await SetAllGames();
+			IsBusy = false;
 		}
 
+		private async Task SetAllGames()
+		{
+			string query = App.MTSChrzanowFirebaseUrl;
+			string json = await new WebClient().DownloadStringTaskAsync(query);
+			var games = JsonConvert.DeserializeObject<List<Game>>(json);
+
+			Games = new ObservableCollection<Game>(games);
+		}
 		private async void OnGameItemClicked(Game game)
 		{
 			await _navigation.PushAsync(new GameDetailsPage(game));
