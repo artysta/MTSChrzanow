d5814f0286326479744abcde541b859e5293f8e5
artysta <adrian.kurek93@gmail.com>
2020-08-26 17:49:33 +0200

    Add new property

--- MTSChrzanow/ViewModels/GameDetailsViewModel.cs
+++ MTSChrzanow/ViewModels/GameDetailsViewModel.cs
@@ -5,6 +5,8 @@ namespace MTSChrzanow.ViewModels
 	public class GameDetailsViewModel : BaseViewModel
 	{
 		private Game _game;
+		private string _message;
+
 		public Game Game
 		{
 			get => _game;
@@ -14,11 +16,24 @@ namespace MTSChrzanow.ViewModels
 			}
 		}
 
+		public string Message
+		{
+			get => _message;
+			set
+			{
+				SetProperty(ref _message, value);
+			}
+		}
+
 		public GameDetailsViewModel() { }
 
 		public GameDetailsViewModel(Game game)
 		{
 			Game = game;
+			string date = $"{ game.DateOfGame.Day }/{ game.DateOfGame.Month }/{ game.DateOfGame.Year }";
+			string time = $"{ game.DateOfGame.Hour }:{ game.DateOfGame.Minute }";
+			Message = game.IsFinished ? $"Mecz odbył się { date } o godzinie { time }."
+									  : $"Mecz się jeszcze nie odbył. Spotkanie będzie miało miejsce { date } o godzinie  { time }.";
 		}
 	}
 }
