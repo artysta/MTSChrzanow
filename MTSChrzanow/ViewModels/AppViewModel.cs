c9c24257c151705208b375c5d058f9a9817cfc30
artysta <adrian.kurek93@gmail.com>
2020-09-09 17:53:33 +0200

    Add viewmodel for app page

---
+++ MTSChrzanow/ViewModels/AppViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MTSChrzanow.Models;
+using Xamarin.Essentials;
+
+namespace MTSChrzanow.ViewModels
+{
+	public class AppViewModel : BaseViewModel
+	{
+		public User LoggedUser { get; set; }
+
+		public AppViewModel()
+		{
+			LoggedUser = new User()
+			{
+				Email = Preferences.Get("REMEMBERED_USER", "N/A")
+			};
+		}
+	}
+}
