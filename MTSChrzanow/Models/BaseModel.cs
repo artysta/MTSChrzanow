2c6d0777bec76c306c66d884fe02a76982d02192
artysta <adrian.kurek93@gmail.com>
2020-09-11 14:31:56 +0200

    Create base class for models

---
+++ MTSChrzanow/Models/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace MTSChrzanow.Models
+{
+	public class BaseModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void RaisePropertyChanged(string name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+}
+		
