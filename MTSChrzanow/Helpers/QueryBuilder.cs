729bf59d995e46bb8292e0ee3bda27fa02478ea9
artysta <adrian.kurek93@gmail.com>
2020-09-13 20:55:58 +0200

    Add query builder class

---
+++ MTSChrzanow/Helpers/QueryBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace MTSChrzanow.Helpers
+{
+	class QueryBuilder
+	{
+		public static string CreateQuery(params string[] p)
+		{
+			StringBuilder builder = new StringBuilder();
+			foreach (string s in p) builder.Append(s);
+			return builder.ToString();
+		}
+	}
+}
