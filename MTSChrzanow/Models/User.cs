a57fb50f39ce504d793408c975b7d4357fb27b18
artysta <adrian.kurek93@gmail.com>
2020-09-16 22:09:05 +0200

    Add new properties & constructor

--- MTSChrzanow/Models/User.cs
+++ MTSChrzanow/Models/User.cs
@@ -3,5 +3,13 @@
 	public class User
 	{
 		public string Email { get; set; }
+		public string Password { get; set; }
+		public string Token { get; set; }
+
+		public User(string email, string password)
+		{
+			Email = email;
+			Password = password;
+		}
 	}
 }
